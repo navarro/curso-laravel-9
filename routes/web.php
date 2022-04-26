787c0c954a2a2790ac4460aba753c805bc342d58
Marcos Navarro <marcoscnavarro@gmail.com>
2022-04-26 00:12:48 -0300

    git commit -m "aula 7"

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\{
+    UserController
+};
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/', function () {
     return view('welcome');
